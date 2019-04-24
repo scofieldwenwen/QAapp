5cc80a5d550057d3254ba7561aa525108e19f5a0
scofield_pc_win7 <scofieldwenwen@163.com>
2019-04-24 16:21:18 +0800

    login ui

--- src/app/pages/login/login.page.spec.ts
+++ src/app/pages/login/login.page.spec.ts
@@ -1,21 +1,22 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TabNotificationPage } from './tab-notification.page';
+import { LoginPage } from './login.page';
 
-describe('TabNotificationPage', () => {
-  let component: TabNotificationPage;
-  let fixture: ComponentFixture<TabNotificationPage>;
+describe('LoginPage', () => {
+  let component: LoginPage;
+  let fixture: ComponentFixture<LoginPage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [TabNotificationPage],
+      declarations: [ LoginPage ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    }).compileComponents();
+    })
+    .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TabNotificationPage);
+    fixture = TestBed.createComponent(LoginPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/tab-more/tab-more.page.ts
+++ src/app/tab-more/tab-more.page.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-tab-more',
   templateUrl: 'tab-more.page.html',
   styleUrls: ['tab-more.page.scss']
 })
-export class TabMorePage {}
+
+export class TabMorePage {
+
+  constructor(public router: Router) {
+
+  }
+
+  goLoginPage() {
+    this.router.navigateByUrl('/login');
+  }
+}
